5a5da95a9c214a5915e5f0d04d4715efaab259b7
Néstor Nápoles López <napulen@gmail.com>
2020-12-11 15:17:19 -0500

    Sorting the name of files in the zip, just for consistency

--- dataset.py
+++ dataset.py
@@ -2,14 +2,15 @@ from urllib.request import urlretrieve
 from zipfile import ZipFile
 
 
-def downloadAndExtract(listFile="dataset.txt"):
+def downloadAndExtract(listFile=None):
     """Downloads and extracts all RomanText files in When-in-Rome.
 
     The script gets the latest master branch of the When-in-Rome repository,
-    extracts all the RomanText files into a local folder, and generates
-    a file `dataset.txt` with a global path to every file it extracted.
+    extracts all the RomanText files into a local folder, and returns
+    a list with a global path to every file it extracted.
 
-    The name of the listFile generated can be optionally provided.
+    If `listFile` is provided, the list of files is also written in disk,
+    in the location provided by listFile.
     """
     wheninromeURL = (
         "https://github.com/MarkGotham/When-in-Rome/archive/master.zip"
@@ -20,13 +21,15 @@ def downloadAndExtract(listFile="dataset.txt"):
     # TODO: Maybe handle any issues based on httpResponse
     repo = ZipFile(tmpZipFile)
     directory = []
-    for f in repo.namelist():
+    for f in sorted(repo.namelist()):
         if "analysis" in f and "feedback_on" not in f and f.endswith("txt"):
             localFileName = repo.extract(f)
             directory.append(localFileName)
-    with open(listFile, "w") as fout:
-        fout.write("\n".join(directory))
+    if listFile:
+        with open(listFile, "w") as fout:
+            fout.write("\n".join(directory))
+    return directory
 
 
 if __name__ == "__main__":
-    downloadAndExtract()
+    downloadAndExtract(listFile="dataset.txt")
