1693e4d6e4a356fade42247cd826a9808be66534
Néstor Nápoles López <napulen@gmail.com>
2020-12-12 22:26:35 -0500

    Formatting comments so that they pass linting

--- unconventional_chords.py
+++ unconventional_chords.py
@@ -1,5 +1,5 @@
 unconventional_chords = {
-    ("major", "#ivobb7b5"): "It",  ## to check
+    ("major", "#ivobb7b5"): "It",  # to check
     ("major", "I32"): "I",
     ("major", "I532"): "I",
     ("major", "I54"): "I",
@@ -9,8 +9,8 @@ unconventional_chords = {
     ("major", "IV532"): "IV",
     ("major", "IV732"): "IV7",
     ("major", "IV7532"): "IV7",
-    ("major", "IV763"): "IV7",  ## to check
-    ("major", "IV765"): "IV7",  ## to check
+    ("major", "IV763"): "IV7",  # to check
+    ("major", "IV765"): "IV7",  # to check
     ("major", "I[add4]"): "I",
     ("major", "I[no3no5]"): "I",
     ("major", "V42[no3]"): "V2",
@@ -18,7 +18,7 @@ unconventional_chords = {
     ("major", "V43[add9]"): "V43",
     ("major", "V43[no3]"): "V43",
     ("major", "V54"): "V",
-    ("major", "V6543"): "V65",  ## to check
+    ("major", "V6543"): "V65",  # to check
     ("major", "V65M9"): "V65",
     ("major", "V65add9"): "V65",
     ("major", "V6[add9]"): "V65",
@@ -26,7 +26,7 @@ unconventional_chords = {
     ("major", "V7532"): "V7",
     ("major", "V7532/IV"): "V7/IV",
     ("major", "V7532/vi"): "V7/vi",
-    ("major", "V765"): "V7",  ## to check
+    ("major", "V765"): "V7",  # to check
     ("major", "V7M9"): "V7",
     ("major", "V7[add9]"): "V7",
     ("major", "V7[no3]"): "V7",
@@ -51,51 +51,51 @@ unconventional_chords = {
     ("major", "i54"): "i",
     ("major", "i[add4]"): "i",
     ("major", "ii43[no3]"): "ii43",
-    ("major", "ii7532"): "ii7",  ## to check
-    ("major", "ii754"): "ii7",  ## to check
+    ("major", "ii7532"): "ii7",  # to check
+    ("major", "ii754"): "ii7",  # to check
     ("major", "iii42[no5]"): "iii2",
     ("major", "iii65[add9]"): "iii65",
-    ("major", "iii7532"): "iii7",  ## to check
+    ("major", "iii7532"): "iii7",  # to check
     ("major", "iii9"): "iii7",
     ("major", "iio64[no3]"): "iio64",
     ("major", "iv4"): "iv",
-    ("major", "iv63#3[add#4]"): "iv6",  ## to check
+    ("major", "iv63#3[add#4]"): "iv6",  # to check
     ("major", "iv7b2"): "iv7",
     ("major", "vi65[no5]"): "vi65",
-    ("major", "vi7542"): "vi7",  ## to check
+    ("major", "vi7542"): "vi7",  # to check
     ("major", "viio43[no3]/vi"): "viio43/vi",
     ("major", "viio65[no5]"): "viio65",
-    ("major", "viio742"): "viio7",  ## to check
+    ("major", "viio742"): "viio7",  # to check
     ("major", "viio7[no5]/vi"): "viio7/vi",
-    ("major", "viiob75/V"): "viio7/V",  ## to check
+    ("major", "viiob75/V"): "viio7/V",  # to check
     ("major", "viiø7532"): "viiø7",
     ("major", "viiø7[no3]/V"): "viiø7/V",
-    ("minor", "#ivob7b5"): "It",  ## to check
-    ("minor", "#vi75"): "#vi7",  ## to check
-    ("minor", "#vi75b4"): "#vi7",  ## to check
+    ("minor", "#ivob7b5"): "It",  # to check
+    ("minor", "#vi75"): "#vi7",  # to check
+    ("minor", "#vi75b4"): "#vi7",  # to check
     ("minor", "I7b2"): "I7",
-    ("minor", "II7542"): "II7",  ## to check
-    ("minor", "III+654"): "III+",  ## to check
-    ("minor", "III+742"): "III+",  ## to check
+    ("minor", "II7542"): "II7",  # to check
+    ("minor", "III+654"): "III+",  # to check
+    ("minor", "III+742"): "III+",  # to check
     ("minor", "III42[no3]"): "III2",
     ("minor", "IV9"): "IV7",
     ("minor", "It53[add4]"): "It",
-    ("minor", "V32"): "V",  ## to check
+    ("minor", "V32"): "V",  # to check
     ("minor", "V4"): "V",
     ("minor", "V42[no3no5]"): "V2",
-    ("minor", "V432"): "V43",  ## to check
-    ("minor", "V6432"): "V64",  ## to check
+    ("minor", "V432"): "V43",  # to check
+    ("minor", "V6432"): "V64",  # to check
     ("minor", "V64[no3]"): "V64",
-    ("minor", "V654"): "V65",  ## to check
-    ("minor", "V6543"): "V65",  ## to check
-    ("minor", "V65b33/v"): "V65/v",  ## to check
-    ("minor", "V73"): "V7",  ## to check
-    ("minor", "V73/iv"): "V7/iv",  ## to check
-    ("minor", "V732"): "V7",  ## to check
-    ("minor", "V742"): "V7",  ## to check
+    ("minor", "V654"): "V65",  # to check
+    ("minor", "V6543"): "V65",  # to check
+    ("minor", "V65b33/v"): "V65/v",  # to check
+    ("minor", "V73"): "V7",  # to check
+    ("minor", "V73/iv"): "V7/iv",  # to check
+    ("minor", "V732"): "V7",  # to check
+    ("minor", "V742"): "V7",  # to check
     ("minor", "V7532"): "V7",
-    ("minor", "V7642"): "V7",  ## to check
-    ("minor", "V765"): "V7",  ## to check
+    ("minor", "V7642"): "V7",  # to check
+    ("minor", "V765"): "V7",  # to check
     ("minor", "V7[add4]"): "V7",
     ("minor", "V7[no3]"): "V7",
     ("minor", "V7[no5]"): "V7",
@@ -110,42 +110,42 @@ unconventional_chords = {
     ("minor", "V[no3no5]"): "V",
     ("minor", "Vb9"): "V",
     ("minor", "bVII[add2]"): "bVII",
-    ("minor", "bvi54"): "bvi",  ## to check
+    ("minor", "bvi54"): "bvi",  # to check
     ("minor", "i54"): "i",
-    ("minor", "i62"): "i6",  ## to check
+    ("minor", "i62"): "i6",  # to check
     ("minor", "i64[no3]"): "i64",
     ("minor", "i6[no1]"): "i6",
     ("minor", "i[no3no5]"): "i",
     ("minor", "ii54"): "ii",
-    ("minor", "ii542"): "ii",  ## to check
-    ("minor", "ii754"): "ii7",  ## to check
+    ("minor", "ii542"): "ii",  # to check
+    ("minor", "ii754"): "ii7",  # to check
     ("minor", "ii9"): "ii7",
-    ("minor", "ii9b5"): "iio7",  ## to check
+    ("minor", "ii9b5"): "iio7",  # to check
     ("minor", "iio43[no3]"): "iio43",
     ("minor", "iio65[no5]"): "iio65",
-    ("minor", "iio752"): "iio7",  ## to check
+    ("minor", "iio752"): "iio7",  # to check
     ("minor", "iiø42[no3]"): "iiø42",
     ("minor", "iiø65[no5]"): "iiø65",
     ("minor", "iiø7532"): "iiø7",
     ("minor", "iiø7b5"): "iiø7",
     ("minor", "iv4"): "iv",
-    ("minor", "iv543"): "iv",  ## to check
-    ("minor", "iv632"): "iv6",  ## to check
-    ("minor", "iv654"): "iv65",  ## to check
-    ("minor", "iv732"): "iv7",  ## to check
+    ("minor", "iv543"): "iv",  # to check
+    ("minor", "iv632"): "iv6",  # to check
+    ("minor", "iv654"): "iv65",  # to check
+    ("minor", "iv732"): "iv7",  # to check
     ("minor", "iv74"): "iv7",
-    ("minor", "iv764"): "iv764",  ## to check
+    ("minor", "iv764"): "iv764",  # to check
     ("minor", "iv7[no3]"): "iv7",
     ("minor", "iv9"): "iv7",
     ("minor", "iv[no3no5]"): "iv",
     ("minor", "v4"): "v",
     ("minor", "viio432"): "viio43",
     ("minor", "viio43[no3]"): "viio43",
-    ("minor", "viio62"): "viio6",  ## to check
-    ("minor", "viio6432"): "viio6",  ## to check
-    ("minor", "viio6553"): "viio6",  ## to check
+    ("minor", "viio62"): "viio6",  # to check
+    ("minor", "viio6432"): "viio6",  # to check
+    ("minor", "viio6553"): "viio6",  # to check
     ("minor", "viio65[no5]/V"): "viio65/V",
-    ("minor", "viio7642"): "viio7",  ## to check
+    ("minor", "viio7642"): "viio7",  # to check
     ("minor", "viio7[no3]/v"): "viio7/v",
     ("minor", "vo64[no3]"): "vo64",
 }
